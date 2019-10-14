279d0a3513801cef938464bdcc8d5850cbbd2b37
avbelyshev <avbelishev@gmail.com>
2019-10-14 15:16:10 +0300

    Install Quasar

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,11 +2,15 @@ const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 const pug = require('./loaders/pug')
+const babelLoaderQuasar = require('./loaders/babel-loader-quasar')
+const stylus = require('./loaders/stylus')
 const aliases = require('./utils/aliases')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.loaders.prepend('pug', pug)
+environment.loaders.append('js', babelLoaderQuasar)
+environment.loaders.append('stylus', stylus)
 
 environment.config.merge(aliases)
 module.exports = environment
