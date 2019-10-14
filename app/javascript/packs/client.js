8198831ebeff670019f8dae65640a3fbf0a0832f
avbelyshev <avbelishev@gmail.com>
2019-10-14 23:20:53 +0300

    Fix import of Quasar

--- app/javascript/packs/client.js
+++ app/javascript/packs/client.js
@@ -1,53 +1,7 @@
 import Vue from 'vue'
 import App from '../client.vue'
 import { backend } from '../api/index.js'
-
-import iconSet from 'quasar/icon-set/fontawesome-v5.js'
-import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
-
-import '../assets/styles/quasar.styl'
-import 'quasar/dist/quasar.ie.polyfills'
-
-import {
-  Quasar,
-  QLayout,
-  QHeader,
-  QDrawer,
-  QPageContainer,
-  QPage,
-  QToolbar,
-  QToolbarTitle,
-  QBtn,
-  QSpace,
-  QTable,
-  QForm,
-  QInput,
-  QSpinnerHourglass
-} from 'quasar'
-
-Vue.use(Quasar, {
-  config: {},
-  components: {
-    QLayout,
-    QHeader,
-    QDrawer,
-    QPageContainer,
-    QPage,
-    QToolbar,
-    QToolbarTitle,
-    QBtn,
-    QSpace,
-    QTable,
-    QForm,
-    QInput,
-    QSpinnerHourglass
-  },
-  directives: {
-  },
-  iconSet: iconSet,
-  plugins: {
-  }
-})
+import Quasar from '../quasar/index.js'
 
 Vue.prototype.$backend = backend
 
---
+++ app/javascript/quasar/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+
+import iconSet from 'quasar/icon-set/fontawesome-v5.js'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+
+import '../assets/styles/quasar.styl'
+import 'quasar/dist/quasar.ie.polyfills'
+
+import {
+    Quasar,
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QSpace,
+    QTable,
+    QForm,
+    QInput,
+    QSpinnerHourglass
+} from 'quasar'
+
+Vue.use(Quasar, {
+    config: {},
+    components: {
+        QLayout,
+        QHeader,
+        QDrawer,
+        QPageContainer,
+        QPage,
+        QToolbar,
+        QToolbarTitle,
+        QBtn,
+        QSpace,
+        QTable,
+        QForm,
+        QInput,
+        QSpinnerHourglass
+    },
+    directives: {
+    },
+    iconSet: iconSet,
+    plugins: {
+    }
+})
